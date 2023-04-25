f4ddcfc75192c25009b244bb11e0d39d7f363c59
Matthew Knorr <matt@knorrs.net>
2023-04-24 18:51:01 -0600

    Prep 3 - 5 work

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 101); 
+        Console.WriteLine(randomNumber);
+        int guess = -1;
+        while (guess != randomNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (randomNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (randomNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+
+        }
     }
 }
