a6e473c3ce5e670c4d52924a0273bc4f1abb98a5
Matthew Knorr <matt@knorrs.net>
2023-05-03 17:50:13 -0600

    Code for Week 3

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job jobOne = new Job();
+        jobOne._jobTitle = "Student";
+        jobOne._company = "BYU-I";
+        jobOne._startYear = 2021;
+        jobOne._endYear = 2024;
+        jobOne.Display();
+        
+        Job jobTwo = new Job();
+        jobTwo._jobTitle = "Barista";
+        jobTwo._company = "Starbucks";
+        jobTwo._startYear = 2019;
+        jobTwo._endYear = 2023;
+        jobTwo.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Matthew Knorr";
+
+        myResume._jobs.Add(jobOne);
+        myResume._jobs.Add(jobTwo);
+
+        myResume.Display();
+
+         
+
     }
+
+    
 }
