048de5a901775ed0b9fa0579c5626a9e22ff9ef2
Matthew Knorr <matt@knorrs.net>
2023-07-08 15:27:14 -0600

    Bug and path fixes

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,122 @@
 using System;
+using System.Collections.Generic;
+
+class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Length { get; set; }
+    public List<Comment> Comments { get; set; }
+
+    public Video(string title, string author, int length)
+    {
+        Title = title;
+        Author = author;
+        Length = length;
+        Comments = new List<Comment>();
+    }
+
+    public int GetNumberOfComments()
+    {
+        return Comments.Count;
+    }
+}
+
+class Comment
+{
+    public string Name { get; set; }
+    public string Text { get; set; }
+
+    public Comment(string name, string text)
+    {
+        Name = name;
+        Text = text;
+    }
+}
+
+class RandomNumberGenerator
+{
+    private Random random;
+
+    public RandomNumberGenerator()
+    {
+        random = new Random();
+    }
+
+    public int GenerateNumber(int minValue, int maxValue)
+    {
+        return random.Next(minValue, maxValue + 1);
+    }
+}
+
+class RandomNameSelector
+{
+    private Random random;
+    private List<string> names;
+
+    public RandomNameSelector(List<string> nameList)
+    {
+        random = new Random();
+        names = nameList;
+    }
+
+    public string SelectRandomName()
+    {
+        int index = random.Next(names.Count);
+        return names[index];
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Create videos
+        List<Video> videos = new List<Video>();
+
+        RandomNumberGenerator rng = new RandomNumberGenerator();
+        int randomNumberOne = rng.GenerateNumber(800, 2500);
+
+        List<string> names = new List<string> { "ShadowSlayer", "RapidFire", "NinjaGamer", "SavageQueen", "CyberWolf", "PhantomStrike", "EpicGamer", "PixelMaster", "StealthSniper", "LunarKnight", "Frostbite", "XenoBlade", "ThunderStrike", "SpectralSorcerer", "VenomousViper" };
+        RandomNameSelector nameSelector = new RandomNameSelector(names);
+        string randomNameOne = nameSelector.SelectRandomName();
+
+        Video video1 = new Video("How to Parse a File Python 3", randomNameOne, randomNumberOne);
+        video1.Comments.Add(new Comment(nameSelector.SelectRandomName(), "Comment 1 for video 1"));
+        video1.Comments.Add(new Comment(nameSelector.SelectRandomName(), "Comment 2 for video 1"));
+        video1.Comments.Add(new Comment(nameSelector.SelectRandomName(), "Comment 3 for video 1"));
+        videos.Add(video1);
+
+        int randomNumberTwo = rng.GenerateNumber(125, 500);
+        string randomNameTwo = nameSelector.SelectRandomName();
+
+        Video video2 = new Video("Video 2", randomNameTwo, randomNumberTwo);
+        video2.Comments.Add(new Comment(nameSelector.SelectRandomName(), "Comment 1 for video 2"));
+        video2.Comments.Add(new Comment(nameSelector.SelectRandomName(), "Comment 2 for video 2"));
+        videos.Add(video2);
+
+        int randomNumberThree = rng.GenerateNumber(400, 1300);
+        string randomNameThree = nameSelector.SelectRandomName();
+
+        Video video3 = new Video("Video 3", randomNameThree, randomNumberThree);
+        video3.Comments.Add(new Comment(nameSelector.SelectRandomName(), "Comment 1 for video 3"));
+        videos.Add(video3);
+
+        // Display video information
+        foreach (Video video in videos)
+        {
+            Console.WriteLine("Title: " + video.Title);
+            Console.WriteLine("Author: " + video.Author);
+            Console.WriteLine("Length: " + video.Length + " seconds");
+            Console.WriteLine("Number of comments: " + video.GetNumberOfComments());
+
+            Console.WriteLine("Comments:");
+            foreach (Comment comment in video.Comments)
+            {
+                Console.WriteLine(" - " + comment.Name + ": " + comment.Text);
+            }
+
+            Console.WriteLine();
+        }
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,225 @@
 using System;
+using System.Collections.Generic;
+
+class Order
+{
+    private List<Product> products;
+    private Customer customer;
+
+    public Order(List<Product> products, Customer customer)
+    {
+        this.products = products;
+        this.customer = customer;
+    }
+
+    public decimal CalculateTotalCost()
+    {
+        decimal totalCost = 0;
+        foreach (Product product in products)
+        {
+            totalCost += product.Price * product.Quantity;
+        }
+
+        // Add shipping cost based on customer location
+        if (customer.IsInUSA())
+        {
+            totalCost += 5;
+        }
+        else
+        {
+            totalCost += 35;
+        }
+
+        return totalCost;
+    }
+
+    public string GetPackingLabel()
+    {
+        string packingLabel = "Packing Label:\n";
+        foreach (Product product in products)
+        {
+            packingLabel += $"Product ID: {product.ProductId}, Name: {product.Name}\n";
+        }
+
+        return packingLabel;
+    }
+
+    public string GetShippingLabel()
+    {
+        string shippingLabel = "Shipping Label:\n";
+        shippingLabel += $"Name: {customer.Name}\n";
+        shippingLabel += customer.Address.GetFormattedAddress();
+
+        return shippingLabel;
+    }
+}
+
+class Product
+{
+    private string name;
+    private string productId;
+    private decimal price;
+    private int quantity;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public string ProductId
+    {
+        get { return productId; }
+        set { productId = value; }
+    }
+
+    public decimal Price
+    {
+        get { return price; }
+        set { price = value; }
+    }
+
+    public int Quantity
+    {
+        get { return quantity; }
+        set { quantity = value; }
+    }
+}
+
+class Customer
+{
+    private string name;
+    private Address address;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public Address Address
+    {
+        get { return address; }
+        set { address = value; }
+    }
+
+    public bool IsInUSA()
+    {
+        return address.IsInUSA();
+    }
+}
+
+class Address
+{
+    private string streetAddress;
+    private string city;
+    private string state;
+    private string country;
+
+    public string StreetAddress
+    {
+        get { return streetAddress; }
+        set { streetAddress = value; }
+    }
+
+    public string City
+    {
+        get { return city; }
+        set { city = value; }
+    }
+
+    public string State
+    {
+        get { return state; }
+        set { state = value; }
+    }
+
+    public string Country
+    {
+        get { return country; }
+        set { country = value; }
+    }
+
+    public bool IsInUSA()
+    {
+        return country == "USA";
+    }
+
+    public string GetFormattedAddress()
+    {
+        string formattedAddress = $"Street Address: {streetAddress}\n";
+        formattedAddress += $"City: {city}\n";
+        formattedAddress += $"State/Province: {state}\n";
+        formattedAddress += $"Country: {country}";
+
+        return formattedAddress;
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        // Create products
+        Product product1 = new Product
+        {
+            Name = "Product 1",
+            ProductId = "P1",
+            Price = 10,
+            Quantity = 2
+        };
+
+        Product product2 = new Product
+        {
+            Name = "Product 2",
+            ProductId = "P2",
+           Price = 15,
+            Quantity = 3
+        };
+
+        // Create customers
+        Address address1 = new Address
+        {
+            StreetAddress = "123 Main Street",
+            City = "New York",
+            State = "NY",
+            Country = "USA"
+        };
+
+        Customer customer1 = new Customer
+        {
+            Name = "Customer 1",
+            Address = address1
+        };
+
+        Address address2 = new Address
+        {
+            StreetAddress = "456 Elm Street",
+            City = "Los Angeles",
+            State = "CA",
+            Country = "USA"
+        };
+
+        Customer customer2 = new Customer
+        {
+            Name = "Customer 2",
+            Address = address2
+        };
+
+        // Create orders
+        Order order1 = new Order(new List<Product> { product1, product2 }, customer1);
+        Order order2 = new Order(new List<Product> { product2 }, customer2);
+
+        // Display order details
+        Console.WriteLine("Order 1:");
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine("Total Cost: $" + order1.CalculateTotalCost());
+        Console.WriteLine();
+
+        Console.WriteLine("Order 2:");
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine("Total Cost: $" + order2.CalculateTotalCost());
     }
 }
